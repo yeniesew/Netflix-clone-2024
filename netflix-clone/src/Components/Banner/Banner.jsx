d56c98d5a0cfde324a4172960978ba08c69be141
yeniesew <yebrielij@gmail.com>
2025-03-26 08:21:43 -0700

    banner section

--- netflix-clone/src/Components/Banner/Banner.jsx
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,54 +1,52 @@
-import React, { useEffect, useState } from "react";
-import axios from "../../utils/axios";
-import requests from "../../utils/requests";
-import "./banner.css";
+import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from '../../utils/axios';
+import requests from '../../utils/requests';
+import './banner.css'
 
 const Banner = () => {
-  const [movie, setMovie] = useState({});
-  useEffect(() => {
-    (async () => {
-      try {
-        const request = await axios.get(requests.fetchNetflixOriginals);
-        // console.log(request)
-        setMovie(
-          request.data.results[
-            Math.floor(Math.random() * request.data.results.length)
-          ]
-        );
-      } catch (error) {
-        console.log("error", error);
-      }
-    })();
-  }, []);
+    const [movie, setMovie] = useState({});
+    useEffect(() => {
+        (async () => {
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals);
+                console.log(request);
+                setMovie(
+                    request.data.results[
+                        Math.floor(Math.random() * request.data.results.length)
+                    ]);
+            } catch (error) {
+                console.log("error", error);
+            }
+    })()
+}, []);
 
-  function truncate(str, n) {
+function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
-  }
+}
   return (
-    <div
-      className="banner"
-      style={{
-        backgroundSize: "cover",
-        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-      }}
-    >
-      <div className="banner__contents">
-        <h1 className="banner__title">
-          {movie?.title || movie?.name || movie?.original_name}
-        </h1>
-        <div className="banner__buttons">
-          <button className="banner__button play">Play</button>
-          <button className="banner__button">My List</button>
+    <div className='banner'
+    style={{
+      backgroundSize: 'cover',
+      backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+      backgroundPosition: 'center center',
+      backgroundRepeat: 'no-repeat'
+    }}>
+        <div className="banner-contents">
+            <h1 className="banner-title">
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
+            <div className="banner-buttons">
+                <button className="banner-button play">Play</button>
+                <button className="banner-button">My List</button>
+            </div>
+            <h1 className="banner-description">
+                {truncate(movie?.overview, 150)}
+            </h1>
         </div>
-        <h1 className="banner__description">
-          {truncate(movie?.overview, 150)}
-        </h1>
-      </div>
-      <div className="banner__fadeBottom" />
+        <div className="banner-fadeBottom"/>
     </div>
-  );
-};
+  )
+}
 
 export default Banner;
